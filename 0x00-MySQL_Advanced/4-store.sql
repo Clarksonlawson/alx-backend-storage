7c1c22601bb6a350db1b725a79db517b064a6945
Awomu Clarkson <clarksonlawson1@gmail.com>
2024-07-11 19:18:47 +0000

    g

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,14 @@
+-- Script to create trigger to decrease item quantity after adding an order
+DELIMITER //
+
+CREATE TRIGGER decrease_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END; //
+
+DELIMITER ;
+
