6af278fc65f6e08c00c5347cc52c60a46f778cb4
Awomu Clarkson <clarksonlawson1@gmail.com>
2024-07-11 19:16:51 +0000

    f

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,10 @@
--- Script to list Glam rock bands by longevity
-SELECT band_name, (2022 - formed) AS lifespan
+-- Script to list all bands with Glam rock as their main style, ranked by their longevity
+SELECT
+    band_name,
+    CASE
+        WHEN split IS NOT NULL THEN split - formed
+        ELSE 2022 - formed
+    END AS lifespan
 FROM metal_bands
-WHERE main_style = 'Glam rock'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
-
