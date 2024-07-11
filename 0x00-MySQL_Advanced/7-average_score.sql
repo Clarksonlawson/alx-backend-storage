7a89db3487cbbbe4131876283b249368021112a2
Awomu Clarkson <clarksonlawson1@gmail.com>
2024-07-11 19:26:48 +0000

    d

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,20 @@
+-- Script to create stored procedure to compute average score for a user
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE avg_score FLOAT;
+
+    -- Calculate average score
+    SELECT AVG(score) INTO avg_score
+    FROM corrections
+    WHERE user_id = user_id;
+
+    -- Update user's average score
+    UPDATE users
+    SET average_score = avg_score
+    WHERE id = user_id;
+END; //
+
+DELIMITER ;
+
